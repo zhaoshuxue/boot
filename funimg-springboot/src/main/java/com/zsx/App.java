f54bfc63493a5af233d9865ab1ad8c203216d21a
zhaoshuxue <zhaoshuxue@qding.me>
2018-01-20 18:48:21 +0800

    commitByZsx

---
+++ funimg-springboot/src/main/java/com/zsx/App.java
@@ -0,0 +1,27 @@
+package com.zsx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by highness on 2018/1/7 0007.
+ */
+@Controller
+@EnableAutoConfiguration
+@SpringBootApplication
+public class App {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ funimg-springboot/src/main/java/com/zsx/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.zsx.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by ZSX on 2018/1/20.
+ *
+ * @author ZSX
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("index")
+    public String toIndex(Model model){
+
+        model.addAttribute("name", "xue");
+        return "index";
+    }
+
+}
