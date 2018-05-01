4a798425b2f0fd0fff444232e7b476a8f6da411a
zhaoshuxue <zhaoshuxue@163.com>
2018-05-01 23:56:18 +0800

    commitByZsx

--- miniApp/funimg/pages/everyday/index.js
+++ miniApp/funimg/pages/everyday/index.js
@@ -5,62 +5,71 @@ Page({
    * 页面的初始数据
    */
   data: {
-    dataList:[1,2,3]
+    dataList: [{
+      "title": "0501：阿萨德法师法士大夫",
+      "imgUrl": "http://ww2.sinaimg.cn/mw690/b11e11f9jw1f48rlmqbeig20ak06o4qr.gif"
+    }, {
+      "title": "0430：阿萨德法师法士大夫",
+      "imgUrl": "http://ww1.sinaimg.cn/mw690/b11e11f9jw1f48rlsc2sjg20au06n4qv.gif"
+    }, {
+      "title": "0429：阿萨德法师法士大夫",
+      "imgUrl": "http://ww2.sinaimg.cn/mw690/b11e11f9jw1f48rllabiqg20c806fdz7.gif"
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
