34e9e19cae7a620dc458af745c791de841cec170
zhaoshuxue <zhaoshuxue@163.com>
2018-05-09 00:47:12 +0800

    commitByZsx

--- miniApp/funimg/pages/detail/index.js
+++ miniApp/funimg/pages/detail/index.js
@@ -12,7 +12,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
@@ -22,22 +22,51 @@ Page({
     this.setData({
       dataList: [{
         "title": "0501：阿萨德法师法士大夫阿萨德法师法士大夫阿萨德法师法士大夫阿萨德法师法士大夫阿萨德法师法士大夫阿萨德法师法士大夫阿萨德法师法士大夫阿萨德法师法士大夫阿萨德法师法士大夫阿萨德法师法士大夫阿萨德法师法士大夫阿萨德法师法士大夫阿萨德法师法士大夫阿萨德法师法士大夫阿萨德法师法士大夫阿萨德法师法士大夫阿萨德法师法士大夫阿萨德法师法士大夫阿萨德法师法士大夫阿萨德法师法士大夫",
-        "imgUrl": "http://highness.qiniudn.com/tiaoqilai.gif"
+        "imgList": [
+          { "imgUrl": "http://highness.qiniudn.com/tiaoqilai.gif" }
+        ]
       }, {
         "title": "0430：阿萨德法师法士大夫",
-        "imgUrl": "http://ww1.sinaimg.cn/mw690/b11e11f9jw1f48rlsc2sjg20au06n4qv.gif"
+
+        "imgList": [
+          {
+            "imgUrl": "http://ww1.sinaimg.cn/mw690/b11e11f9jw1f48rlsc2sjg20au06n4qv.gif"
+          }
+        ]
       }, {
         "title": "0429：阿萨德法师法士大夫",
-        "imgUrl": "http://ww2.sinaimg.cn/mw690/b11e11f9jw1f48rllabiqg20c806fdz7.gif"
+        "imgList": [
+          {
+            "imgUrl": "http://ww2.sinaimg.cn/mw690/b11e11f9jw1f48rllabiqg20c806fdz7.gif"
+          },
+          {
+            "imgUrl": "http://ww2.sinaimg.cn/mw690/b11e11f9jw1f48rllabiqg20c806fdz7.gif"
+          }
+        ]
       }, {
         "title": "0429：阿萨德法师法士大夫",
-        "imgUrl": "/images/1.jpg"
+        "imgList": [
+          {
+            "imgUrl": "/images/1.jpg"
+          }
+        ]
       }, {
         "title": "0429：阿萨德法师法士大夫",
-        "imgUrl": "/images/2.jpg"
+        "imgList": [
+          {
+            "imgUrl": "/images/2.jpg"
+          }
+        ]
       }, {
         "title": "0429：阿萨德法师法士大夫",
-        "imgUrl": "/images/3.jpg"
+        "imgList": [
+          {
+            "imgUrl": "/images/3.jpg"
+          },
+          {
+            "imgUrl": "/images/3.jpg"
+          }
+        ]
       }]
     })
   },
@@ -56,35 +85,35 @@ Page({
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
 
   //
@@ -97,7 +126,7 @@ Page({
   },
 
 
-  imageLoad: function(e){
+  imageLoad: function (e) {
     // console.log(e)
   }
 })
